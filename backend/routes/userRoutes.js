2a4f2879e4c80e5a99ed9acab4bd87327d038ed0
Rajat Jain <rajat17rj@gmail.com>
2021-01-29 21:49:48 +0530

    Added Frontend User Authentication

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.post('/', registerUser)
 router.post('/login', authUser)
 router.get('/profile', protect, getUserProfile)
+router.put('/profile', protect, updateUserProfile)
 
 export default router
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,9 @@ import Header from './components/Header'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/register' component={RegisterScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
